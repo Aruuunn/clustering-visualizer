aaefc96974d61c998253010b70a4466c928c6e13
Arun Murugan <arunmurugan200@gmail.com>
2020-08-23 14:02:06 +0530

    made the fab into a collection and added a expand fab

--- src/views/KMEANS/components/InfoModal/index.tsx
+++ src/views/KMEANS/components/InfoModal/index.tsx
@@ -52,6 +52,7 @@ function InfoModal(props: Props): ReactElement {
     const sm = useMediaQuery(theme.breakpoints.up('sm'));
 
     const [open, setOpen] = useState<boolean>(false);
+    const [expand, setExpand] = useState<boolean>(true);
     const [mode, setMode] = useState<Mode>(Mode.INFO); //show either result or info
 
     const [page, setPage] = useState<number>(0);
@@ -70,28 +71,7 @@ function InfoModal(props: Props): ReactElement {
     if (!open) {
         return (
             <Grow in={!open}>
-                <div>
-                    {' '}
-                    {props.kmeans.info !== null && props.global.start === false ? (
-                        <BlueFab
-                            disabled={info === null}
-                            onClick={() => {
-                                setOpen((s) => !s);
-                                setMode(Mode.RESULT);
-                            }}
-                            style={{ position: 'fixed', bottom: sm ? '30vh' : '30vh', right: 20 }}
-                        >
-                            <SvgIcon>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                                    <path d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                        d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm-2 14l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z"
-                                        fill="white"
-                                    />
-                                </svg>
-                            </SvgIcon>
-                        </BlueFab>
-                    ) : null}
+                {props.kmeans.mode === KMEANSMode.SingleIteration ? (
                     <Fab
                         disabled={info === null}
                         color="secondary"
@@ -99,7 +79,12 @@ function InfoModal(props: Props): ReactElement {
                             setOpen((s) => !s);
                             setMode(Mode.INFO);
                         }}
-                        style={{ position: 'fixed', bottom: sm ? '45vh' : 20, right: 20 }}
+                        style={{
+                            margin: '10px',
+                            position: 'fixed',
+                            bottom: sm ? '60vh' : 20,
+                            right: 20,
+                        }}
                     >
                         <SvgIcon>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
@@ -108,7 +93,119 @@ function InfoModal(props: Props): ReactElement {
                             </svg>
                         </SvgIcon>
                     </Fab>
-                </div>
+                ) : (
+                    <div>
+                        {expand ? (
+                            <Grid
+                                container
+                                direction="column"
+                                justify="space-between"
+                                alignItems="center"
+                                style={{
+                                    position: 'fixed',
+                                    bottom: sm ? '60vh' : 20,
+                                    right: 20,
+                                    width: 'auto',
+                                }}
+                            >
+                                {props.kmeans.info !== null &&
+                                props.global.start === false &&
+                                props.kmeans.mode === KMEANSMode.MultipleIteration ? (
+                                    <Grow in={true}>
+                                        <BlueFab
+                                            disabled={info === null}
+                                            onClick={() => {
+                                                setOpen((s) => !s);
+                                                setMode(Mode.RESULT);
+                                            }}
+                                            style={{ margin: '10px' }}
+                                        >
+                                            <SvgIcon>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    width="24"
+                                                >
+                                                    <path d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z" />
+                                                </svg>
+                                            </SvgIcon>
+                                        </BlueFab>
+                                    </Grow>
+                                ) : null}
+                                <Grow in={true}>
+                                    <div>
+                                        <Fab
+                                            disabled={info === null}
+                                            color="secondary"
+                                            onClick={() => {
+                                                setOpen((s) => !s);
+                                                setMode(Mode.INFO);
+                                            }}
+                                            style={{ margin: '10px' }}
+                                        >
+                                            <SvgIcon>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    width="24"
+                                                >
+                                                    <path d="M0 0h24v24H0z" fill="none" />
+                                                    <path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z" />
+                                                </svg>
+                                            </SvgIcon>
+                                        </Fab>
+                                    </div>
+                                </Grow>
+
+                                <SvgIcon style={{ margin: '10px' }} fontSize="large" onClick={() => setExpand(false)}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="white"
+                                        width="24"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none" />
+                                        <path
+                                            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                                            fill="white"
+                                        />
+                                    </svg>
+                                </SvgIcon>
+                            </Grid>
+                        ) : (
+                            <Grid
+                                container
+                                direction="column"
+                                justify="space-between"
+                                alignItems="center"
+                                style={{
+                                    position: 'fixed',
+                                    bottom: sm ? '60vh' : 20,
+                                    right: 20,
+                                    width: 'auto',
+                                }}
+                            >
+                                <Fab color="secondary" onClick={() => setExpand(true)}>
+                                    <SvgIcon fontSize="large">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            width="24"
+                                        >
+                                            <path d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+                                        </svg>
+                                    </SvgIcon>
+                                </Fab>
+                            </Grid>
+                        )}
+                    </div>
+                )}
             </Grow>
         );
     }
