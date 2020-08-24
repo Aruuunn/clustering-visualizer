95bd3d2bf9326fc7a0598c7c532f450e7d1e2f6f
Arun Murugan <arunmurugan200@gmail.com>
2020-08-24 21:41:38 +0530

    fixed a minor error in dragging fab, caused by recent commits

--- src/views/KMEANS/components/InfoModal/index.tsx
+++ src/views/KMEANS/components/InfoModal/index.tsx
@@ -79,7 +79,7 @@ function InfoModal(props: Props): ReactElement {
 
         const move = (e: any) => {
             const X: number = e.clientX - 34;
-            let y: number = e.clientY - 29;
+            let y: number = e.clientY - 210;
 
             y = y < 60 ? 60 : y;
 
@@ -118,21 +118,6 @@ function InfoModal(props: Props): ReactElement {
                                         height: '300px',
                                     }}
                                 >
-                                    <Zoom in={true}>
-                                        <Fab onPointerDown={handleMove} style={{ backgroundColor: 'white' }}>
-                                            <SvgIcon>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    height="24"
-                                                    viewBox="0 0 24 24"
-                                                    width="24"
-                                                >
-                                                    <path d="M0 0h24v24H0V0z" fill="none" />
-                                                    <path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-                                                </svg>
-                                            </SvgIcon>
-                                        </Fab>
-                                    </Zoom>
                                     {props.kmeans.info !== null &&
                                     props.global.start === false &&
                                     props.kmeans.mode === KMEANSMode.MultipleIteration ? (
@@ -184,7 +169,21 @@ function InfoModal(props: Props): ReactElement {
                                             </Fab>
                                         </div>
                                     </Zoom>
-
+                                    <Zoom in={true}>
+                                        <Fab onPointerDown={handleMove} style={{ backgroundColor: 'white' }}>
+                                            <SvgIcon>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    width="24"
+                                                >
+                                                    <path d="M0 0h24v24H0V0z" fill="none" />
+                                                    <path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+                                                </svg>
+                                            </SvgIcon>
+                                        </Fab>
+                                    </Zoom>
                                     <SvgIcon
                                         style={{ margin: '10px' }}
                                         fontSize="large"
